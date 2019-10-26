0a73254e8af0d0a2c15839ca733ea0ffe9d448b9
ckbr0 <45081053+ckbr0@users.noreply.github.com>
2019-10-27 00:29:56 +0200

    Update void-post-install.sh

--- void-post-install.sh
+++ void-post-install.sh
@@ -1,6 +1,6 @@
 readonly BASE_PKGS="dialog cryptsetup lvm2 mdadm"
 readonly X_PKGS="$BASE_PKGS xorg-minimal xorg-input-drivers xorg-video-drivers setxkbmap xauth font-misc-misc terminus-font dejavu-fonts-ttf alsa-plugins-pulseaudio intel-ucode"
-readonly DWM_PKGS="$X_PKGS xorg-fonts libX11-devel libXft-devel fontconfig-devel xinit font-inconsolata-otf font-libertine-otf font-awesome xinit arandr noto-fonts-emoji font-symbola xclip xdotool xdpyinfo xdpyinfo xsetroot xset pam xss-lock xsecurelock compton dmenu feh pywal tabbed xtools lf calcurse libnotify dbus dunst sxiv neovim mpd mpc mpv ncmpcpp newsboat sc-im htop unrar unzip p7zip youtube-dl zathura zathura-djvu zathura-pdf-mupdf poppler mediainfo mediainfo atool fzf simple-mtpfs bash-completion zsh zsh-syntax-highlighting neomutt transmission scrot qrencode pamixer pulsemixer firefox steam lutris gnuplot base-devel clang tcc python python3 python3-pip ninja gromit-mpx wxMaxima octave"
+readonly DWM_PKGS="$X_PKGS xorg-fonts libX11-devel libXft-devel fontconfig-devel libXinerama-devel xinit font-inconsolata-otf font-libertine-otf font-awesome xinit arandr noto-fonts-emoji font-symbola xclip xdotool xdpyinfo xdpyinfo xsetroot xset pam xss-lock xsecurelock compton dmenu feh pywal tabbed xtools lf calcurse libnotify dbus dunst sxiv neovim mpd mpc mpv ncmpcpp newsboat sc-im htop unrar unzip p7zip youtube-dl zathura zathura-djvu zathura-pdf-mupdf poppler mediainfo mediainfo atool fzf simple-mtpfs bash-completion zsh zsh-syntax-highlighting neomutt transmission scrot qrencode pamixer pulsemixer firefox steam lutris gnuplot base-devel clang tcc python python3 python3-pip ninja gromit-mpx wxMaxima octave"
 
 # Enable 32bit repos
 sudo xbps-install -Sy void-repo-multilib
@@ -10,7 +10,7 @@ sudo xbps-install -Sy void-repo-nonfree
 sudo xbps-install -Sy void-repo-multilib-nonfree
 sudo echo XBPS_ALLOW_RESTRICTED=yes >> etc/conf
 
-sudo xbps-install -S "$DWM_PKGS"
+sudo xbps-install -S $DWM_PKGS
 
 readonly TMPDIR=$(mktemp --tmpdir="$(pwd -P)" -d)
 
@@ -40,3 +40,5 @@ git clone https://github.com/ckbr0/ozadja.git $HOME/slike/ozadja
 sudo rm -rf $HOME/slike/ozadja/.git
 
 ./$HOME/.local/bin/updatewal $HOME/slike/ozadja
+
+sudo rm -r $TMPDIR
