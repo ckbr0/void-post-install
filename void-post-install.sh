c7aaa062905e4ff29d8cf3355b6f006f591633a8
ckbr0 <ckbr@tuta.io>
2019-10-27 00:02:54 +0200

    first commit

---
+++ void-post-install.sh
@@ -0,0 +1,36 @@
+readonly BASE_PKGS="dialog cryptsetup lvm2 mdadm"
+readonly X_PKGS="$BASE_PKGS xorg-minimal xorg-input-drivers xorg-video-drivers setxkbmap xauth font-misc-misc terminus-font dejavu-fonts-ttf alsa-plugins-pulseaudio intel-ucode"
+readonly DWM_PKGS="$X_PKGS xorg-fonts libX11-devel libXft-devel fontconfig-devel xinit font-inconsolata-otf font-libertine-otf font-awesome xinit arandr noto-fonts-emoji font-symbola xclip xdotool xdpyinfo xdpyinfo xsetroot xset pam xss-lock xsecurelock compton dmenu feh pywal tabbed xtools lf calcurse libnotify dbus dunst sxiv neovim mpd mpc mpv ncmpcpp newsboat sc-im htop unrar unzip p7zip youtube-dl zathura zathura-djvu zathura-pdf-mupdf poppler mediainfo mediainfo atool fzf simple-mtpfs bash-completion zsh zsh-syntax-highlighting neomutt transmission scrot qrencode pamixer pulsemixer firefox steam lutris gnuplot base-devel clang tcc python python3 python3-pip ninja gromit-mpx wxMaxima octave"
+
+# Enable 32bit repos
+sudo xbps-install -Sy void-repo-multilib
+
+# Install nonfree repos and add option to install restricted packages
+sudo xbps-install -Sy void-repo-nonfree
+sudo xbps-install -Sy void-repo-multilib-nonfree
+sudo echo XBPS_ALLOW_RESTRICTED=yes >> etc/conf
+
+sudo xbps-install -S "$DWM_PKGS"
+
+readonly TMPDIR=$(mktemp --tmpdir="$(pwd -P)" -d)
+
+mkdir $HOME/razvoj
+mkdir $HOME/prenosi
+mkdir $HOME/slike
+mkdir $HOME/videoposnetki
+mkdir $HOME/namizje
+mkdir $HOME/glasba
+mkdir $HOME/dokumenti
+mkdir $HOME/šola
+
+# Dotfiles
+git clone https://github.com/ckbr0/linuxrice.git $TMPDIR/dotfiles
+sudo rm -r $TMPDIR/dotfiles/.git
+cp -rf $TMPDIR/dotfiles/. $HOME/
+
+# Wallpapers
+mkdir $HOME/slike/ozadja
+git clone https://github.com/ckbr0/ozadja.git $HOME/slike/ozadja
+sudo rm -rf $HOME/slike/ozadja/.git
+
+./$HOME/.local/bin/updatewal $HOME/slike/ozadja
